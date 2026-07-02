277fe66128db99b78498556537dee1f34202dac8
MachinTruc-truc <jeanguy.galliot@edu.igensia.com>
2026-07-02 08:56:30 +0200

    Refactor tests for calcul function
    Refactor test imports and update test cases.

--- app.test.js
+++ app.test.js
@@ -1,9 +1,5 @@
-const calcul = require('./app');
+const { calcul } = require('./app');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(calcul(1, 2)).toBe(3);
-});
-
-test('adds 15 + 2 to equal 17 (a > 10)', () => {
-  expect(calcul(15, 2)).toBe(17);
+test('2 + 3 = 5', () => {
+  expect(calcul(2, 3)).toBe(5);
 });
