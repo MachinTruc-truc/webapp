e60d952b926865784e45025935ff83d5de3bec44
MachinTruc-truc <jeanguy.galliot@edu.igensia.com>
2026-07-02 08:41:26 +0200

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -2,3 +2,7 @@ const calcul = require('./app');
 test('adds 1 + 2 to equal 3', () => {
   expect(calcul(1, 2)).toBe(3);
 });
+
+test('adds 15 + 2 to equal 17 (a > 10)', () => {
+  expect(calcul(15, 2)).toBe(17);
+});
